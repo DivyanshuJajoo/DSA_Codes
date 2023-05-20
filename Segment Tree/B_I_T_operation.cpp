3b39f5ab50ac020b457e37ae078030da9c261f06
DivyanshuJajoo <103714090+DivyanshuJajoo@users.noreply.github.com>
2023-05-20 15:37:08 +0530

    Add files via upload

---
+++ Segment Tree/B_I_T_operation.cpp
@@ -0,0 +1,56 @@
+//video check for revision
+#include <iostream> 
+
+using namespace std; 
+
+ 
+
+
+void updateBIT(int BITree[], int n, int index, int val) 
+{ 
+    
+    index = index + 1; 
+  
+     
+    while (index <= n) 
+    { 
+   
+    BITree[index] += val; 
+  
+   
+    index += index & (-index); //to update all those index which are affected by given arr[i]
+    } 
+}
+ 
+int *constructBITree(int arr[], int n) 
+{ 
+	
+	int *BITree = new int[n+1]; 
+	for (int i=1; i<=n; i++) 
+		BITree[i] = 0; 
+
+	 
+	for (int i=0; i<n; i++) 
+		updateBIT(BITree, n, i, arr[i]); 
+
+	
+
+	return BITree; 
+} 
+
+
+ 
+int main() 
+{ 
+	int freq[] = {10, 20, 30, 40, 50, 60, 70, 80, 90}; 
+	int n = sizeof(freq)/sizeof(freq[0]); 
+	int *BITree = constructBITree(freq, n); 
+	
+	updateBIT(BITree, n, 2, 35); 
+		
+	
+
+	
+	
+	return 0; 
+} 
